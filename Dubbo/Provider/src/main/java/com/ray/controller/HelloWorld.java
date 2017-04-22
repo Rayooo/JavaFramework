c6918e25d2c4b896174418596c62f3f79588f32a
Ray <chenxiaolei.ray@gmail.com>
2017-04-22 14:23:56 +0800

    Spring Boot Hello World例子

---
+++ Dubbo/Provider/src/main/java/com/ray/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.ray.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Hello World";
+    }
+
+}
